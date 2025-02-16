90d002515c53596485643992ae7e055123c5ab50
mahesh <maheshghartla@gmail.com>
2025-02-16 09:17:49 +0530

    Added comments and improved code readability

---
+++ script.py
@@ -0,0 +1,8 @@
+# This script prints a welcome message to the console
+
+def greet():
+    """Function to print a greeting message"""
+    print("Hello, World! Welcome to my Git commit challenge.")
+
+# Call the function
+greet()
